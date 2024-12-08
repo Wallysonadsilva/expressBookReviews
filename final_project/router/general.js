c7e07bbd0b588ff5d1bcd37cb816df4fad62e525
wallysonadsilva <wallysonadsilva@gmail.com>
2024-12-08 12:09:08 -0500

    Task 3: completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,10 +29,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+  
+    // Filter the books to find those with the matching author
+    const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
+  
+    if (booksByAuthor.length > 0) {
+      return res.status(200).json(booksByAuthor);
+    } else {
+      return res.status(404).json({message: "No books found for the specified author"});
+    }
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
