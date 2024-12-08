b193fcb23afffd20a4b819a84383fd5d459a0b89
wallysonadsilva <wallysonadsilva@gmail.com>
2024-12-08 12:25:11 -0500

    Task 4 & 5: completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,7 +32,6 @@ public_users.get('/isbn/:isbn',function (req, res) {
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
   
-    // Filter the books to find those with the matching author
     const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
   
     if (booksByAuthor.length > 0) {
@@ -44,14 +43,32 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const booksBytitle = Object.values(books).filter(book => book.title.toLocaleLowerCase() === title.toLocaleLowerCase());
+
+    if(booksBytitle.length > 0){
+        return res.status(200).json(booksBytitle);
+    } else {
+        return res.status(404).json({message: "No books found for the specifeid title"});
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+
+    if(book){
+        const reviews = book.reviews;
+
+        if(Object.keys(reviews).length > 0){
+            return res.status(200).json(reviews);
+        } else {
+            return res.status(403).json({message: " No reviews available for this book"});
+        }
+    } else {
+        return res.status(404).json({message: "Book not found"});
+    }
 });
 
 module.exports.general = public_users;
